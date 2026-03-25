e43249bdf9468a2075ff0de62d648ad5b984f0cf
ThienGB <thienlove161203@gmail.com>
2026-03-25 16:56:18 +0700

    feat: Add Joi validation middleware and a utility function to pick object properties.

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,11 +1,16 @@
 const Joi = require('joi');
 const ApiError = require('../utils/ApiError');
+const pick = require('../utils/pick');
 
+/**
+ * Middleware để validate request chống lại schema đã định nghĩa.
+ * Tự động bóc tách body, query, hoặc params để validate.
+ */
 const validate = (schema) => (req, res, next) => {
-  const validSchema = typeof schema === 'function' ? schema(Joi) : schema;
+  const validSchema = pick(schema, ['params', 'query', 'body']);
+  const object = pick(req, Object.keys(validSchema));
   
-  const object = req.body;
-  const { value, error } = Joi.object(validSchema)
+  const { value, error } = Joi.compile(validSchema)
     .prefs({ errors: { label: 'key' }, abortEarly: false })
     .validate(object);
 
@@ -13,6 +18,8 @@ const validate = (schema) => (req, res, next) => {
     const errorMessage = error.details.map((details) => details.message).join(', ');
     return next(new ApiError(400, errorMessage));
   }
+  
+  // Gán lại các giá trị đã được sanitize/validate vào request
   Object.assign(req, value);
   return next();
 };
